c40255c0dd906bf04de4573dc17e0a8cdc2d066f
ajohnsoon <allanjohnson.leal@gmail.com>
2022-09-21 19:43:58 -0300

    feat: create project properties

---
+++ src/main/java/application/Run.java
@@ -0,0 +1,7 @@
+package main.java.application;
+
+public class Run {
+    public static void main(String[] args){
+        System.out.println("Running");
+    }
+}
---
+++ src/main/java/application/db/config/DB.java
@@ -0,0 +1,82 @@
+package main.java.application.db.config;
+
+import main.java.application.db.exceptions.DbException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+
+        if(connection == null){
+            try{
+                Properties properties = loadProperties();
+                String url = properties.getProperty("dburl");
+                connection = DriverManager.getConnection(url, properties);
+            }
+            catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException{
+        if(connection != null){
+            try {
+                connection.close();
+            }
+            catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement statement){
+        if(statement != null){
+            try{
+                statement.close();
+            }
+            catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResults(ResultSet resultSet){
+        try{
+            if(resultSet != null){
+                resultSet.close();
+            }
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closePreparedStatement(PreparedStatement preparedStatement){
+        try{
+            if(preparedStatement != null){
+                preparedStatement.close();
+            }
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    private static Properties loadProperties(){
+        try (FileInputStream fs = new FileInputStream("./src/resources/config/db.properties")){
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }
+        catch (IOException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+}
