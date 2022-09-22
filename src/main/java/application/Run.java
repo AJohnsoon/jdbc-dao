4517c08531e8b4b5390cc6f30ae7e03f2be81095
ajohnsoon <allanjohnson.leal@gmail.com>
2022-09-22 11:21:53 -0300

    feat: add seller entitie

--- src/main/java/application/Run.java
+++ src/main/java/application/Run.java
@@ -1,10 +1,15 @@
 package main.java.application;
 
 import main.java.application.model.entities.Department;
+import main.java.application.model.entities.Seller;
+
+import java.util.Date;
 
 public class Run {
     public static void main(String[] args){
         Department dp = new Department(1, "Books");
+        Seller seller = new Seller(1, "Alex Green", "alexgreen@teste.com", new Date(), 3000.00, dp);
         System.out.println(dp);
+        System.out.println(seller);
     }
 }
