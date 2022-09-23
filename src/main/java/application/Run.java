c06845cf7859e9d8371af470be151a62f6c08795
ajohnsoon <allanjohnson.leal@gmail.com>
2022-09-23 00:17:35 -0300

    feat: add implementation insert a new seller

--- src/main/java/application/Run.java
+++ src/main/java/application/Run.java
@@ -6,29 +6,40 @@ import main.java.application.model.entities.Department;
 import main.java.application.model.entities.Seller;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Run {
-    public static void main(String[] args) throws SQLException {
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static void main(String[] args) throws SQLException, ParseException {
         System.out.println("_______FindByID_________");
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller sellerId = sellerDao.findById(3);
         System.out.println(sellerId);
-        System.out.println("________________");
 
-        System.out.println("_______FindBuyDepartmentID_________");
-        Department dp = new Department(3, null);
+        System.out.println();
+        System.out.println("_______FindByDepartmentID_________");
+        Department dp = new Department(4, null);
         List<Seller> listFindByDepartmentId = sellerDao.findByDepartment(dp);
         for (Seller obj: listFindByDepartmentId) {
             System.out.println(obj);
         }
 
-
+        System.out.println();
         System.out.println("_______FindAll_________");
         List<Seller> listFindAll = sellerDao.findAll();
         for (Seller obj: listFindAll) {
             System.out.println(obj);
         }
+
+        Date date = sdf.parse("27/03/2002");
+        System.out.println("_______Insert Seller_________");
+        Seller insertSeller = new Seller(null, "John Teal", "johnt@teste.com", date, 1827.75, dp);
+        sellerDao.insert(insertSeller);
+        System.out.println("Inserted! New id is: "+ insertSeller.getId());
     }
 }
